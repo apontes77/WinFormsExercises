6df9f51c95485c55eb7b38a36e1526a2e7d22d62
apontes77 <alexandrepontesgyn@gmail.com>
2021-05-19 22:08:19 -0300

    interface grafica do trabalho em andamento

---
+++ trabalhofinal/toDoApp/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toDoApp
+{
+    class Restaurant
+    {
+        public long id { get; set; }
+        public string restaurantName { get; set; }
+        public string sloganRestaurant { get; set; }
+
+        public Restaurant()
+        {
+                
+        }
+
+       
+      
+    }
+}
