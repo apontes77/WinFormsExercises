f47c83a6b521113bf4a0fcf908ffd589825391cd
apontes77 <alexandrepontesgyn@gmail.com>
2021-05-30 23:05:56 -0300

    finaliza ex2

--- ex2/urnaEletronica/Form3.Designer.cs
+++ ex2/urnaEletronica/Form3.Designer.cs
@@ -300,6 +300,7 @@ namespace urnaEletronica
             this.button2.TabIndex = 1;
             this.button2.Text = "CORRIGE";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -311,6 +312,7 @@ namespace urnaEletronica
             this.button1.TabIndex = 0;
             this.button1.Text = "BRANCO";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button14
             // 
